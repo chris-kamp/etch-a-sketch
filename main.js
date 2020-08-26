077521016181be043d97a4b2bac427d01472baec
Chris Kamp <chriskamp101@gmail.com>
2020-08-26 21:20:17 +1000

    Allow custom grid size and add reset button

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 //Get a reference to the container for the grid
-let gridContainer = document.getElementById("grid-container");
+const gridContainer = document.getElementById("grid-container");
+const resetButton = document.getElementById("reset-button");
 
-//Create 16*16 grid items
-for (let i = 0; i < 16*16; i++) {
+//Initialise a 16*16 grid on page load
+initialiseGrid(16);
+resetButton.addEventListener("click", clearShading);
+
+//Create a grid item
+function createGridItem() {
     let newDiv = document.createElement("div");
     newDiv.classList.add("grid-item");
     //Shade each grid item on mouseover
@@ -11,8 +16,30 @@ for (let i = 0; i < 16*16; i++) {
     gridContainer.appendChild(newDiv);
 }
 
-//A function to shade a given grid item
+//Shade a given grid item
 function shade() {
     this.classList.add("grid-item-shaded");
 }
+
+//Clear all shaded grid items
+function clearShading() {
+    shaded = document.querySelectorAll(".grid-item-shaded");
+    shaded.forEach(element => {
+        element.classList.remove("grid-item-shaded");
+    })
+}
+
+//Prompt the user for grid dimensions
+
+
+//Initialise a grid of given dimensions
+function initialiseGrid(size) {
+    gridContainer.setAttribute("display", "grid");
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        createGridItem();
+    }
+}
+
 
