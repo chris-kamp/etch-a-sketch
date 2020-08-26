9ecac49f1ed0e90153816ba48a38a2a970ff3408
Chris Kamp <chriskamp101@gmail.com>
2020-08-26 21:35:29 +1000

    Add button to reset grid with custom size

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-//Get a reference to the container for the grid
+//Get a reference to the container for the grid and the clear and reset buttons
 const gridContainer = document.getElementById("grid-container");
 const resetButton = document.getElementById("reset-button");
+const clearButton = document.getElementById("clear-button");
 
 //Initialise a 16*16 grid on page load
 initialiseGrid(16);
-resetButton.addEventListener("click", clearShading);
+
+//When clear or reset button is clicked, clear shading or initialise a new grid respectively
+resetButton.addEventListener("click", resetGrid);
+clearButton.addEventListener("click", clearShading);
 
 //Create a grid item
 function createGridItem() {
@@ -23,14 +27,34 @@ function shade() {
 
 //Clear all shaded grid items
 function clearShading() {
-    shaded = document.querySelectorAll(".grid-item-shaded");
+    const shaded = document.querySelectorAll(".grid-item-shaded");
     shaded.forEach(element => {
         element.classList.remove("grid-item-shaded");
     })
 }
 
-//Prompt the user for grid dimensions
+//Prompt the user for grid size
+function promptSize() {
+    return prompt("Test");
+}
 
+//Delete all grid items
+function deleteGrid() {
+    const grid = document.querySelectorAll(".grid-item");
+    grid.forEach(item => {
+        item.remove();
+    })
+}
+
+//Reset the grid by prompting for new dimensions, deleting the existing grid and initialising a new grid
+function resetGrid() {
+    //prompt for new grid size
+    const size = promptSize();
+    //delete existing grid items
+    deleteGrid();
+    //initialise a new grid of the correct size
+    initialiseGrid(size);
+}
 
 //Initialise a grid of given dimensions
 function initialiseGrid(size) {
